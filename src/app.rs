a0bfffc981bebb4496fe27b3b98155b090f6843f
Andrei Brizhinev <bragovo@gmail.com>
2025-08-30 12:24:26 +0300

    Minimal server

---
+++ src/app.rs
@@ -0,0 +1,36 @@
+use axum::{Router, routing::get};
+use bzd_lib::{error::Error, settings::Settings as _};
+use tracing::info;
+
+use crate::app::{settings::AppSettings, state::AppState};
+
+mod auth;
+mod settings;
+mod state;
+
+pub async fn run() -> Result<(), Error> {
+    let settings = AppSettings::new()?;
+    let state = AppState::new(settings).await?;
+
+    http(&state).await?;
+
+    Ok(())
+}
+
+async fn http(state: &AppState) -> Result<(), Error> {
+    let router = Router::new()
+        .nest(
+            "/api",
+            Router::new()
+                .route("/healthz", get(|| async {}))
+                .nest("/auth", auth::router()),
+        )
+        .with_state(state.to_owned());
+
+    let listener = tokio::net::TcpListener::bind(&state.settings.http.endpoint).await?;
+
+    info!("app: started on {}", listener.local_addr()?);
+    axum::serve(listener, router).await?;
+
+    Ok(())
+}
