965f9f24e123c40584e6ff0fa304047476cb8d7a
Andrei Brizhinev <bragovo@gmail.com>
2025-10-20 18:23:37 +0300

    Add source endpoint

--- src/app/sources.rs
+++ src/app/sources.rs
@@ -1,12 +1,18 @@
 use std::collections::HashSet;
 
-use axum::{Router, extract::State, routing::get};
-use bzd_users_api::{GetSourcesRequest, GetUsersRequest};
+use axum::{
+    Router,
+    extract::State,
+    routing::{get, post},
+};
+use bzd_users_api::{CreateSourceRequest, GetSourcesRequest, GetUsersRequest};
 
 use crate::app::{error::AppError, json::AppJson, state::AppState, user::AppUser};
 
 pub fn router() -> Router<AppState> {
-    Router::new().route("/", get(get_sources))
+    Router::new()
+        .route("/", get(get_sources))
+        .route("/", post(create_source))
 }
 
 async fn get_sources(
@@ -161,6 +167,49 @@ mod get_sources {
                 abbr: user.abbr().into(),
                 color: user.color().into(),
             })
+        }
+    }
+}
+
+async fn create_source(
+    State(AppState {
+        sources_service_client,
+        ..
+    }): State<AppState>,
+    user: AppUser,
+    AppJson(req): AppJson<create_source::Request>,
+) -> Result<AppJson<create_source::Response>, AppError> {
+    let res = sources_service_client
+        .clone()
+        .create_source(CreateSourceRequest {
+            user_id: Some(user.user_id.into()),
+            source_user_id: Some(req.user_id.into()),
+        })
+        .await?
+        .into_inner();
+
+    Ok(AppJson(res.into()))
+}
+
+mod create_source {
+    use bzd_users_api::CreateSourceResponse;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Deserialize)]
+    pub struct Request {
+        pub user_id: String,
+    }
+
+    #[derive(Serialize)]
+    pub struct Response {
+        pub source_id: String,
+    }
+
+    impl From<CreateSourceResponse> for Response {
+        fn from(res: CreateSourceResponse) -> Self {
+            Self {
+                source_id: res.source_id().into(),
+            }
         }
     }
 }
