5de33b8f27284741a716764788e0a8f7089e2bb1
Andy Braga <bragovo@gmail.com>
2025-11-30 16:15:36 +0300

    Add update_topic_user (#7)
    * Add update_topic_user
    
    * Remove debug

--- src/app/users.rs
+++ src/app/users.rs
@@ -218,19 +218,21 @@ mod get_user_topics {
 
     #[derive(Serialize)]
     pub struct Response {
-        pub topics: Vec<Topic>,
+        topics: Vec<Topic>,
     }
 
     #[derive(Serialize)]
-    pub struct Topic {
-        pub topic_id: String,
-        pub title: String,
-        pub topic_user: Option<TopicUser>,
+    struct Topic {
+        topic_id: String,
+        title: String,
+        topic_user: Option<TopicUser>,
     }
 
     #[derive(Serialize)]
-    pub struct TopicUser {
-        pub topic_user_id: String,
+    struct TopicUser {
+        topic_user_id: String,
+        rate: String,
+        timing: String,
     }
 
     type Responses = (GetUserTopicsResponse, GetTopicsUsersResponse);
@@ -267,6 +269,8 @@ mod get_user_topics {
                 title: topic.title().into(),
                 topic_user: topics_users.get(topic.topic_id()).map(|it| TopicUser {
                     topic_user_id: it.topic_user_id().into(),
+                    rate: it.rate().as_str_name().into(),
+                    timing: it.timing().as_str_name().into(),
                 }),
             })
         }
